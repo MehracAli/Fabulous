24e907898f86f6b9f440d6d44f21bca0fdbfd652
Mehrac Mammadov <mehrajvm@code.edu.az>
2023-02-09 20:17:20 +0400

    Header done

---
+++ assets/js/header.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    // nav-bar
+    $(".accesories").on("mouseover",()=>{
+        let drop = $(".accesories-drop")
+        drop.slideDown()
+    })
+    $(".accesories").on("mouseleave",()=>{
+        let drop = $(".accesories-drop")
+        drop.slideToggle(50)
+    })
+    $(".drone").on("mouseover",()=>{
+        let drop = $(".drone-drop")
+        drop.slideDown()
+    })
+    $(".drone").on("mouseleave",()=>{
+        let drop = $(".drone-drop")
+        drop.slideToggle(50)
+    })
+
+    // search
+    $(".search svg").on("click", ()=>{
+        let drop = $(".search input")
+        drop.slideToggle(600)
+    })
+    // login
+    $(".login svg").on("click", ()=>{
+        let drop = $(".login-drop")
+        drop.slideToggle()
+    })
+
+    $(window).on('scroll',function(){
+        if($(window).scrollTop() > 100){
+            let scroll = $('header').addClass('scroll-header')
+            scroll.slideDown("low")
+        }
+        else{
+            let scroll =  $('header').removeClass('scroll-header')
+            scroll.slideUp()
+        }
+    })
+})
