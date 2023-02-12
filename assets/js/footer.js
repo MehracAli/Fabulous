5be7efbdc58d4b355bc602b210997b5c514856a1
Mehrac Mammadov <mehrajvm@code.edu.az>
2023-02-13 01:50:49 +0400

    responsive for 12 pro

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -13,4 +13,15 @@ $(document).ready(function(){
     e.preventDefault();
     $('html, body').animate({scrollTop:0}, 50);
     });
+    //footer-mid
+    $(".footer-mid-sm .sect").on("click", function () {
+        let next = $(this).next();
+        let icon = $(this).find("i");
+    
+        next.slideToggle();
+        icon.toggleClass("active-i");
+    
+        $(".footer-mid-sm .ul").not(next).slideUp()
+        $(".footer-mid-sm .sect i").not(icon).removeClass("active-i")
+      });
 })
