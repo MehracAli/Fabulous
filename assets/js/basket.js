33fed6d8301ba51078e63db8429783fb2116d526
Mehrac Mammadov <mehrajvm@code.edu.az>
2023-02-14 04:10:46 +0400

    basket

--- assets/js/basket.js
+++ assets/js/basket.js
@@ -4,42 +4,45 @@ let basketList = document.querySelector(".basket-list");
 document.addEventListener("DOMContentLoaded", function () {
   let basketStr = localStorage.getItem("basket");
   let basket = JSON.parse(basketStr);
-
+  
   if (!basket || !basket.length) {
     localStorage.setItem("basket", JSON.stringify([]));
   }
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      let product = GetProductDatas(this);
+      let basketStr = localStorage.getItem("basket");
+      let basket = JSON.parse(basketStr);
+      
+      if (!basket) {
+        localStorage.setItem("basket", JSON.stringify([]));
+        basketStr = localStorage.getItem("basket");
+        basket = JSON.parse(basketStr);
+      }
   
-  basket.forEach(product => {
-    BasketList(product)
-  })
-
-});
-
-buttons.forEach((button) => {
-  button.addEventListener("click", function () {
-    let product = GetProductDatas(this);
-    console.log(product);
-    // let basketStr = localStorage.getItem("basket");
-    // let basket = JSON.parse(basketStr);
-
-    // if (!basket) {
-    //   localStorage.setItem("basket", JSON.stringify([]));
-    //   basketStr = localStorage.getItem("basket");
-    //   basket = JSON.parse(basketStr);
-    // }
-    // let existedProduct = basket.find((pro) => {
-    //   return pro.id == product.id;
-    // });
-    // if (!existedProduct) {
-    //   basket.push(product);
-    //   BasketList(product);
-    // } else {
-    //   existedProduct.count++;
-    // }
-    // ProductCount(basket);
-    // TotalPrice(basket)
-    // basketStr = JSON.stringify(basket);
-    // localStorage.setItem("basket", basketStr);
+      let existedProduct = basket.find((pro) => {
+        return pro.id == product.id;
+      });
+  
+      if(!existedProduct){
+          basket.push(product);
+          BasketList(product)
+      }
+      else{
+            existedProduct.count++;
+      }
+  
+      basketStr = JSON.stringify(basket);
+      localStorage.setItem("basket", basketStr);
+      ProductCount(basket)
+      let totalPrice = TotalPrice(basket)
+      let ecoTax = EcoTax(basket)
+      let vat = VAT(totalPrice, ecoTax)
+      SubTotal(totalPrice, vat)
+      basketStr = JSON.stringify(basket);
+      localStorage.setItem("basket", basketStr);
+    });
   });
 });
 
@@ -53,36 +56,59 @@ function GetProductDatas(button) {
   return product;
 }
 
-// function BasketList(product) {
-//   const productHtml = `
-//   <li class="text" id="parent">
-//     <div class="image">
-//       <img src="${product.src}"></img>
-//     </div>
-//     <div class="describtion">
-//       <span class="drop-title">${product.count}x${product.title}</span>
-//       <span class="drop-price">$${product.price}</span>
-//     </div>
-//     <div class="delete">
-//       <i class="fa-solid fa-circle-minus delete-product"></i>
-//     </div>
-//   </li>
-// `;
-// basketList.innerHTML += productHtml;
-// }
+function BasketList(product) {
+  const productHtml = `
+  <li class="text" id="parent">
+    <div class="image">
+      <img src="${product.src}"></img>
+    </div>
+    <div class="describtion" style="width: 50%;">
+      <span class="drop-title" style="color: #232323;font-size: 14px;font-weight: 400;">${product.count} x ${product.title}</span><br>
+      <span class="drop-price" style="color: #232323;font-size: 16px;">$${product.price}</span>
+    </div>
+    <div class="delete">
+    <i class="bi bi-trash3"></i>
+    </div>
+  </li>
+`;
+  basketList.innerHTML += productHtml;
+  basketList.querySelector(".when-empty").remove()
+}
 
-// function ProductCount(basket) {
-//   let basketCount = document.querySelector(".product-count");
-//   basketCount.innerText = basket.reduce((total, product) => {
-//     return (total += product.count);
-//   },0);
-// }
 
-// function TotalPrice(basket) {
-//   let totalPrice = document.querySelector(".total");
-//   totalPrice.innerHTML = basket.reduce((total, product) => {
-//     return (total += parseInt(product.price));
-//   },0);
-// }
+function ProductCount(basket) {
+  let basketCount = document.querySelector(".product-count");
+  basketCount.innerText = basket.reduce((total, product) => {
+    return (total += product.count);
+  },0);
+}
 
-  
+function SubTotal(totalPrice, vat, ecoTax) {
+  let subTotal = document.querySelector(".cd-btm .sub-total")
+  subTotal.innerHTML = totalPrice - vat
+}
+
+function EcoTax(basket) {
+  let EcoTax = document.querySelector(".cd-btm .eco-tax")
+  EcoTax.innerHTML = basket.reduce((total, product) => {
+    return (total += parseInt(2*product.count));
+  },0);
+  return EcoTax.innerHTML
+}
+
+function VAT(totalPrice, ecoTax) {
+  totalPrice-ecoTax
+  let percent = 20/100
+  let vat = document.querySelector(".cd-btm .vat");
+  vat.innerHTML = basket.reduce((total, product) => {
+    return (total += parseInt(product.price*product.count));
+  },0);
+}
+
+function TotalPrice(basket) {
+  let totalPrice = document.querySelector(".cd-btm .total");
+  totalPrice.innerHTML = basket.reduce((total, product) => {
+    return (total += parseInt(product.price*product.count));
+  },0);
+  return totalPrice.innerHTML
+}
