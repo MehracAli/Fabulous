b09c880a659728b982fb02b2d8545128af8d7c01
Mehrac Mammadov <mehrajvm@code.edu.az>
2023-02-17 19:19:37 +0400

    bugs fixed

--- assets/js/sm-header.js
+++ assets/js/sm-header.js
@@ -21,6 +21,7 @@ $("#mini-head .cart svg").on("click", function(){
     $(".mini-cart-drop")[0].style.display = "block"
     document.querySelector("body").style.overflowY = "hidden"
     document.querySelector(".wrap").style.display = "block"
+    console.log(document.querySelector(".wrap"));
 })
 $(".mini-cart-drop .top i").on("click", function(){
     $(".mini-cart-drop")[0].style.display = "none"
